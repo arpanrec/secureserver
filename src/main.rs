0df2d869c5374e81d9bbd4df75fa7af002486485
Arpan Mandal <me@arpanrec.com>
2025-02-13 00:23:19 +0530

    Router (#23)
    * wip
    
    * wip
    
    * Router working
    
    * Router working
    
    * Router working
    
    * Router working

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,65 @@
-use axum::{routing::get, Router};
+use axum::{
+    body::Body,
+    extract::Path,
+    http::{Request, Response, StatusCode},
+    routing::get,
+    Router,
+};
 use secretsquirrel::physical::{delete, read, write};
 
 #[tokio::main]
 async fn main() {
-    write("test", "Hello, World!safdasfas1").await;
-    println!("{:?}", read("test").await);
-    delete("test").await;
-    // let app = Router::new().route("/", get(|| async { "Hello, World!" }));
-    // let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
-    // axum::serve(listener, app).await.unwrap();
+    let app = Router::new()
+        .route(
+            "/secret/{*key}",
+            get(handle_get).post(handle_post).delete(handle_delete),
+        )
+        .route("/{*key}", axum::routing::any(handle_any));
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn handle_get(Path(path): Path<String>) -> Response<String> {
+    println!("Received request for key: {}", path);
+    let value = read(&path).await;
+    println!("Read value: {:?}", value);
+    let builder = Response::builder();
+    if let Some(value) = value {
+        println!("Found value: {}", value);
+        builder
+            .status(StatusCode::OK)
+            .header("Content-Type", "text/plain")
+            .body(value)
+            .expect("Failed to send response")
+    } else {
+        println!("Not Found");
+        builder
+            .status(StatusCode::NOT_FOUND)
+            .header("Content-Type", "text/plain")
+            .body("".to_string())
+            .expect("Failed to send response")
+    }
+}
+
+async fn handle_post(Path(path): Path<String>, body: String) -> Response<String> {
+    println!("Received request for key: {}", path);
+    println!("Received body: {}", body);
+    write(&path, &body).await;
+    Response::new("".to_string())
+}
+
+async fn handle_delete(Path(path): Path<String>) -> Response<String> {
+    println!("Received request for key: {}", path);
+    delete(&path).await;
+    Response::new("".to_string())
+}
+
+// pub fn any<H, T, S>(handler: H) -> MethodRouter<S, Infallible>
+async fn handle_any(Path(path): Path<String>, request: Request<Body>) -> Response<String> {
+    println!(
+        "Received request for lock with method: {:?}",
+        request.method()
+    );
+    println!("Received request for key: {}", path);
+    Response::new("".to_string())
 }
--- src/physical.rs
+++ src/physical.rs
@@ -2,17 +2,19 @@ use libsql::Builder;
 use libsql::Connection;
 use std::time::{SystemTime, UNIX_EPOCH};
 
-// CREATE TABLE secrets_a3 (
-// id_a3 INTEGER PRIMARY KEY AUTOINCREMENT,
-// key_a3 TEXT NOT NULL,
-// value_a3 TEXT NOT NULL,
-// version_a3 INTEGER DEFAULT (1) NOT NULL,
-// updated_at_a3 INTEGER DEFAULT (-1) NOT NULL,
-// is_deleted_a3 INTEGER DEFAULT (0) NOT NULL
+// CREATE TABLE secrets_d (
+//     id_d INTEGER PRIMARY KEY AUTOINCREMENT,
+//     key_d TEXT NOT NULL,
+//     value_d TEXT NOT NULL,
+//     version_d INTEGER DEFAULT (1) NOT NULL,
+//     updated_at_d INTEGER DEFAULT (-1) NOT NULL,
+//     is_deleted_d INTEGER DEFAULT (0) NOT NULL
 // );
-// CREATE UNIQUE INDEX secrets_a3_key_a3_IDX ON secrets_a3 (key_a3,version_a3);
+// CREATE UNIQUE INDEX secrets_d_key_d_IDX ON secrets_d (key_d,version_d);
 
-async fn establish_connection() -> libsql::Connection {
+const TABLE_NAME: &str = "secrets_d";
+
+async fn establish_connection() -> Connection {
     let url = std::env::var("TURSO_DATABASE_URL").expect("TURSO_DATABASE_URL must be set");
     let token = std::env::var("TURSO_AUTH_TOKEN").expect("TURSO_AUTH_TOKEN must be set");
 
@@ -23,11 +25,10 @@ async fn establish_connection() -> libsql::Connection {
 async fn get_current_version(conn: &Connection, key: &str) -> i64 {
     let mut rows = conn
         .query(
-            "SELECT version_a3 FROM secrets_a3 WHERE key_a3 = ? ORDER BY version_a3 DESC LIMIT 1;",
-            libsql::params![key],
-        )
-        .await
-        .unwrap();
+            &format!(
+                "SELECT version_d FROM {TABLE_NAME} WHERE key_d = ? ORDER BY version_d DESC LIMIT 1;"
+            ),
+            libsql::params![key],).await.unwrap();
     if let Some(row) = rows.next().await.unwrap() {
         row.get(0).unwrap()
     } else {
@@ -38,7 +39,9 @@ async fn get_current_version(conn: &Connection, key: &str) -> i64 {
 pub async fn read(key: &str) -> Option<String> {
     let mut rows = establish_connection().await
         .query(
-            "SELECT value_a3 FROM secrets_a3 WHERE key_a3 = ? AND is_deleted_a3 = 0 ORDER BY version_a3 DESC LIMIT 1;",
+            &format!(
+                "SELECT value_d FROM {TABLE_NAME} WHERE key_d = ? AND is_deleted_d = 0 ORDER BY version_d DESC LIMIT 1;"
+            ),
             libsql::params![key],
         )
         .await
@@ -58,11 +61,13 @@ pub async fn write(key: &str, value: &str) -> () {
         .unwrap()
         .as_secs() as i64;
     conn.execute(
-        "INSERT INTO secrets_a3 (key_a3, value_a3, version_a3, updated_at_a3) VALUES (?, ?, ?, ?);",
+        &format!(
+            "INSERT INTO {TABLE_NAME} (key_d, value_d, version_d, updated_at_d) VALUES (?, ?, ?, ?);"
+        ),
         libsql::params![key, value, next_version, current_epoch_time],
     )
-    .await
-    .unwrap();
+        .await
+        .unwrap();
 }
 
 pub async fn delete(key: &str) -> () {
@@ -72,7 +77,7 @@ pub async fn delete(key: &str) -> () {
         .unwrap()
         .as_secs() as i64;
     conn.execute(
-        "UPDATE secrets_a3 SET is_deleted_a3 = 1, updated_at_a3 = ? WHERE key_a3 = ?;",
+        &format!("UPDATE {TABLE_NAME} SET is_deleted_d = 1, updated_at_d = ? WHERE key_d = ?;"),
         libsql::params![current_epoch_time, key],
     )
     .await
