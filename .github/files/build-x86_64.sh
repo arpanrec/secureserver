72e4208a80775482badaf1a5e5ef5871cfc171eb
arpan <arpan.rec@gmail.com>
2025-02-15 05:11:19 +0530

    fix: remove debug echo and exit from x86_64 build script

--- .github/files/build-x86_64.sh
+++ .github/files/build-x86_64.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-echo "iam running the build script"
-exit 1
-
 mkdir -p /tmp/secretsquirrel && rm -rf /tmp/secretsquirrel/x86_64*
 
 mv ./target/x86_64-unknown-linux-gnu/release/secretsquirrel /tmp/secretsquirrel/x86_64-secretsquirrel
