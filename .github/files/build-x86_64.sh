c2a4da987b5d0fd40804a53d29cfa634440cecb2
arpan <arpan.rec@gmail.com>
2025-02-15 04:43:21 +0530

    feat: fix build script to ensure x86_64 binary is moved correctly

--- .github/files/build-x86_64.sh
+++ .github/files/build-x86_64.sh
@@ -6,3 +6,5 @@ mkdir -p /tmp/secretsquirrel && rm -rf /tmp/secretsquirrel/x86_64*
 mv ./target/x86_64-unknown-linux-gnu/release/secretsquirrel /tmp/secretsquirrel/x86_64-secretsquirrel
 
 cargo build --target x86_64-unknown-linux-gnu --release
+
+mv ./target/x86_64-unknown-linux-gnu/release/secretsquirrel /tmp/secretsquirrel/x86_64-secretsquirrel
