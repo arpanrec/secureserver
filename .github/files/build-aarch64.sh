6ebf169dac844f684d5db3f062216e7766896bc7
arpan <arpan.rec@gmail.com>
2025-02-15 05:21:08 +0530

    fix: update environment variable exports in aarch64 build script

--- .github/files/build-aarch64.sh
+++ .github/files/build-aarch64.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-export DEBIAN_FRONTEND=noninteractive
+DEBIAN_FRONTEND=noninteractive
 sudo apt-get update &&
     sudo apt-get install -y gcc-aarch64-linux-gnu g++-aarch64-linux-gnu libc6-dev-arm64-cross
-
-export CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_LINKER=aarch64-linux-gnu-gcc
-export CC_aarch64_unknown_linux_gnu=aarch64-linux-gnu-gcc
-export CXX_aarch64_unknown_linux_gnu=aarch64-linux-gnu-g++
+CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_LINKER=aarch64-linux-gnu-gcc
+CC_aarch64_unknown_linux_gnu=aarch64-linux-gnu-gcc
+CXX_aarch64_unknown_linux_gnu=aarch64-linux-gnu-g++
 rustup target add aarch64-unknown-linux-gnu
 cargo build --target aarch64-unknown-linux-gnu --release
 
