ab9934bb886d39c8de8f3c23c7a6f75a4336366b
arpan <arpan.rec@gmail.com>
2025-02-15 02:32:53 +0530

    wip

---
+++ .github/files/release.config.cjs
@@ -0,0 +1,60 @@
+module.exports = {
+    branches: ['main'],
+    tagFormat: '${version}',
+    plugins: [
+        [
+            '@semantic-release/commit-analyzer',
+            {
+                preset: 'angular',
+                parserOpts: {
+                    noteKeywords: ['BREAKING CHANGE', 'BREAKING CHANGES', 'BREAKING'],
+                },
+            },
+        ],
+        [
+            '@semantic-release/release-notes-generator',
+            {
+                preset: 'angular',
+                parserOpts: {
+                    noteKeywords: ['BREAKING CHANGE', 'BREAKING CHANGES', 'BREAKING'],
+                },
+                writerOpts: {
+                    commitsSort: ['subject', 'scope'],
+                },
+            },
+        ],
+        [
+            '@semantic-release/exec',
+            {
+                prepareCmd: [
+                    'rm -f CHANGELOG.md',
+                    'chmod +x ./.github/files/build.sh',
+                    './.github/files/build.sh',
+                ].join(' && '),
+            },
+        ],
+        [
+            '@semantic-release/changelog',
+            {
+                changelogFile: 'CHANGELOG.md',
+            },
+        ],
+        [
+            '@semantic-release/git',
+            {
+                assets: ['CHANGELOG.md'],
+                message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+            },
+        ],
+        [
+            '@semantic-release/github',
+            {
+                assets: [
+                    {
+                        path: '/tmp/secretsquirrel/*',
+                    },
+                ],
+            },
+        ],
+    ],
+};
