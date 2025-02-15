66ef6cfd7a72d311fbcd0a3ae39592a7c922a0b5
arpan <arpan.rec@gmail.com>
2025-02-15 05:34:45 +0530

    fix: remove redundant move command in x86_64 build script

--- .github/files/build-x86_64.sh
+++ .github/files/build-x86_64.sh
@@ -3,8 +3,6 @@ set -euo pipefail
 
 mkdir -p /tmp/secretsquirrel && rm -rf /tmp/secretsquirrel/x86_64*
 
-mv ./target/x86_64-unknown-linux-gnu/release/secretsquirrel /tmp/secretsquirrel/x86_64-secretsquirrel
-
 cargo build --target x86_64-unknown-linux-gnu --release
 
 mv ./target/x86_64-unknown-linux-gnu/release/secretsquirrel /tmp/secretsquirrel/x86_64-secretsquirrel
